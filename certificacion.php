9dfd9c0d437c8ce34e9b456bbe4ebe12582b5848
Carlos Velasquez <carlosvelasquez@MacBook-Air-de-Carlos.local>
2019-06-14 22:03:36 -0500

    file testing

---
+++ certificacion.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Certificacion</title>
+</head>
+<body>
+	<h1>Certificacion testing</h1>
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 <body>
 	<h1>Texto desde Camavep para edificioagil</h1>
 	<p>Este es el ambiente de produccion</p>
+	<p>Este es el ambiente de Certificacion</p>
 </body>
 </html>
